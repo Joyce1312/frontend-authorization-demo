2e8825011c9c8c52b7183309d2b5f509de051c63
Joyce1312 <joyce1312183j@gmail.com>
2026-01-05 00:07:10 -0500

    feat: checking users token

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Ducks from "./Ducks";
 import Login from "./Login";
@@ -7,6 +7,8 @@ import Register from "./Register";
 import "./styles/App.css";
 import ProtectedRoute from "./ProtectedRoute";
 import * as auth from "../utils/auth";
+import { setToken, getToken } from "../utils/token";
+import * as api from "../utils/api";
 
 function App() {
   const [userData, setUserData] = useState({ username: "", email: "" });
@@ -46,6 +48,7 @@ function App() {
       .then((data) => {
         // Verify that a jwt is included before logging the user in.
         if (data.jwt) {
+          setToken(data.jwt);
           setUserData(data.user); // save user's data to state
           setIsLoggedIn(true); // log the user in
           navigate("/ducks"); // send them to /ducks
@@ -53,6 +56,26 @@ function App() {
       })
       .catch(console.error);
   };
+
+  useEffect(() => {
+    const jwt = getToken();
+
+    if (!jwt) {
+      return;
+    }
+
+    // Call the function, passing it the JWT.
+    api
+      .getUserInfo(jwt)
+      .then(({ username, email }) => {
+        // If the response is successful, log the user in, save their
+        // data to state, and navigate them to /ducks.
+        setIsLoggedIn(true);
+        setUserData({ username, email });
+        navigate("/ducks");
+      })
+      .catch(console.error);
+  }, []);
 
   return (
     <Routes>
